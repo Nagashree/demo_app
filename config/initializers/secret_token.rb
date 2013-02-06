242ab49243b53507aed03a430be678268ee75cb3
Strata <nagashree.aswath@strata.co.in>
2013-02-06 16:36:56 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a62f43028a86a2f1f0f95fe51cc4c5518fe7398f958fa3d6398b4c77b8be75de8c8715bbc394e13ecc73b7f3629bdbb7e4a82a9818a2fb110204ab07debe8635'
